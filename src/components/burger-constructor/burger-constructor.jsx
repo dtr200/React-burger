87db9d51b45f4d86c1693d13e935c391b1c3b68c
dtr200 <negevski@yandex.ru>
2021-11-06 12:17:28 +0300

    Sprint-1/step-1: NavButton component has been fixed

---
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import styles from './burger-constructor.module.css';
+
+class BurgerConstructor extends Component {
+    render(){
+        return(
+            <section className={styles.burgerConstructor}>
+                constructor
+            </section>
+        )
+    }
+}
+
+export default BurgerConstructor;
