4335fd85e2b848c373bc3756a023155ff86d1772
dtr200 <negevski@yandex.ru>
2021-11-07 15:44:38 +0300

    Sprint-1/step-1: BurgerConstructor bun styles has been bit fixed

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -27,7 +27,7 @@ class BurgerConstructor extends Component {
         data.sort((a, b) => b._id - a._id)
         return(
             <section className={`${styles.burgerConstructor} pt-25 pl-4`}> 
-                <ul className={`mt-0 pr-4`}>
+                <ul className={`${styles.bun} mt-0 pr-4`}>
                     {this.getBun(bun, 'top', '(верх)')}
                 </ul>               
                 <ul className={`${styles.list} pr-2`}>
@@ -48,7 +48,7 @@ class BurgerConstructor extends Component {
                         })
                     }
                 </ul>
-                <ul className={`pr-4`}>
+                <ul className={`${styles.bun} pr-4`}>
                     {this.getBun(bun, 'bottom', '(низ)')}
                 </ul>
                 <div className={`${styles.total} text text_type_digits-medium pt-10`}>
