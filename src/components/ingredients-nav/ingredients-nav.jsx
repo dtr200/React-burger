8c999543fc45489b9f79e3c291f9fa32d4c7a062
dtr200 <negevski@yandex.ru>
2021-11-09 00:46:33 +0300

    Sprint-1/step-1: component has been switched to function component & PropTypes added

--- src/components/ingredients-nav/ingredients-nav.jsx
+++ src/components/ingredients-nav/ingredients-nav.jsx
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from 'prop-types';
 import { Tab } from 
     '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './ingredients-nav.module.css';
 
-class IngredientsNav extends Component {
+const IngredientsNav = ({ active, getTabs, 
+                          clickHandler }) => {
 
-    tabs = this.props.getTabs();
+    const tabs = getTabs();
+                        
+    return(
+        <nav>
+            <ul className={`${styles.list} mb-10`}>
+                { tabs.map((tab, i) => (
+                    <li key={i}>
+                        <Tab value={tab} 
+                            active={tab === active}
+                            onClick={clickHandler}>
+                            {tab}
+                        </Tab>
+                    </li>)) }
+            </ul>
+        </nav>
+    )
+}
 
-    render(){
-        const { active, clickHandler } = this.props;
-
-        return(
-            <nav>
-                <ul className={`${styles.list} mb-10`}>
-                    { this.tabs.map((tab, i) => (
-                        <li key={i}>
-                            <Tab value={tab} 
-                                active={tab === active}
-                                onClick={clickHandler}>
-                                {tab}
-                            </Tab>
-                        </li>)) }
-                </ul>
-            </nav>
-        )
-    }
+IngredientsNav.propTypes = {
+    active: PropTypes.string.isRequired,
+    getTabs: PropTypes.func.isRequired,
+    clickHandler: PropTypes.func.isRequired
 }
 
 export default IngredientsNav;
