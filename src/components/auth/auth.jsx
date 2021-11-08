98c6e8ac1e3773651a7e1be23de43cd5db72e5fe
dtr200 <negevski@yandex.ru>
2021-11-09 00:27:36 +0300

    Sprint-1/step-1: Auth component has been updated with PropTypes

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavButton from "../nav-button/nav-button";
+import PropTypes from 'prop-types';
 
 import styles from './auth.module.css';
 
@@ -8,5 +9,14 @@ const Auth = (props) => {
         <NavButton {...props} />
     )
 }
+
+const authShapeProps = PropTypes.shape({
+    title: PropTypes.string.isRequired, 
+    logo: PropTypes.string.isRequired, 
+    type: PropTypes.string.isRequired, 
+    view: PropTypes.bool.isRequired 
+});
+
+Auth.propTypes = authShapeProps.isRequired;
 
 export default Auth;
