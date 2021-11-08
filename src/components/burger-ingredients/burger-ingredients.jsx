0f32935d6e8ce6dc7327a54cef29e5a055410a26
dtr200 <negevski@yandex.ru>
2021-11-09 00:36:44 +0300

    Sprint-1/step-1: BurgerIngredients switchet to function component & key attributes added

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,28 +1,24 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import IngredientsNav from "../ingredients-nav/ingredients-nav";
 import ItemCart from '../item-cart/item-cart';
 
 import styles from './burger-ingredients.module.css';
 
-class BurgerIngredients extends Component {
+const BurgerIngredients = ({ data }) => {
 
-    state = {
-        tab: 'Булки',
-        selected: []
-    }
+    const [ tab, setTab ] = useState('Булки');
 
-    dict = {
+    const dict = {
         bun: 'Булки',        
         sauce: 'Соусы',
         main: 'Начинки'
     }
 
-    createIngredientsBlocks = () => {
-        const { data } = this.props;
+    const createIngredientsBlocks = () => {
         const blocks = [];
 
-        for(let key in this.dict){
+        for(let key in dict){
             const item = { title: key };
             item.items = data.filter(elem => 
                 elem.type === key)
@@ -31,56 +27,47 @@ class BurgerIngredients extends Component {
         return blocks;
     }
 
-    getTabs = () => 
-        Object.values(this.dict);
+    const getTabs = () => 
+        Object.values(dict);
 
-    onTabClick = (title) => {
-        this.setState({ tab: title });
-    }
+    const onTabClick = (title) =>
+        setTab(title);
+       
+    const blocks = createIngredientsBlocks();
 
-    onSelected = (e) => {
-        const id = e.target.parentNode.dataset.id;
-        console.log(id)
-    }
-
-    render(){        
-        const { tab } = this.state;
-        const blocks = this.createIngredientsBlocks();
-        return(
-            <section className={`${styles.burgerIngredients}`}>
-                <h1 className={`mt-10 mb-5 text text_type_main-large`}>
-                    Соберите бургер
-                </h1>
-                <IngredientsNav 
-                    clickHandler={this.onTabClick}
-                    getTabs={this.getTabs}
-                    active={tab} />
-                <section className={styles.ingredients}>
-                    { blocks.map((block, i) => {
-                        return (
-                            <section key={i}>
-                                <h2 className={`text text_type_main-medium mb-6`} 
-                                    id={block.title}>
-                                    { this.dict[block.title] }
-                                </h2>
-                                <ul className={`${styles.ingredientsList} mt-6 mb-0 pl-4 pr-2`}>
-                                    { block.items.map((item, i) => (
-                                        <ItemCart 
-                                            id={item._id}
-                                            name={item.name}
-                                            price={item.price}
-                                            image={item.image}
-                                            selectItem={this.onSelected}
-                                            key={i} />
-                                        )) }
-                                </ul>
-                            </section>
-                        )
-                    }) }
-                </section>                
-            </section>
-        )
-    }
+    return (
+        <section className={`${styles.burgerIngredients}`}>
+            <h1 className={`mt-10 mb-5 text text_type_main-large`}>
+                Соберите бургер
+            </h1>
+            <IngredientsNav 
+                clickHandler={onTabClick}
+                getTabs={getTabs}
+                active={tab} />
+            <section className={styles.ingredients}>
+                { blocks.map((block, i) => {
+                    return (
+                        <section key={block.items[0]._id + i}>
+                            <h2 className={`text text_type_main-medium mb-6`} 
+                                id={block.title}>
+                                { dict[block.title] }
+                            </h2>
+                            <ul className={`${styles.ingredientsList} mt-6 mb-0 pl-4 pr-2`}>
+                                { block.items.map(item => (
+                                    <ItemCart 
+                                        id={item._id}
+                                        name={item.name}
+                                        price={item.price}
+                                        image={item.image}
+                                        key={item._id} />
+                                    )) }
+                            </ul>
+                        </section>
+                    )
+                }) }
+            </section>                
+        </section>
+    )
 }
 
 const ingredientsShapeTypes = PropTypes.shape({
