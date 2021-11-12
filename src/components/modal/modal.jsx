d86a27905a700621f5cfca2af88be6af1c1914cf
dtr200 <negevski@yandex.ru>
2021-11-12 21:59:59 +0300

    Sprint-1/step-2: Modal component has been fixed

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import { CloseIcon } from 
     '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -8,8 +7,12 @@ import styles from './modal.module.css';
 const Modal = ({ title, children }) => {
     return (
         <div className={`${styles.modal} p-10 pb-15`}>
-            <h3 className='text text_type_main-medium pr-15'>{ title }</h3>
-            <CloseIcon type="primary" />
+            <div className={styles.titleContainer}>
+                <h3 className='text text_type_main-medium pr-15'>
+                    { title }
+                </h3>
+                <CloseIcon type="primary" />
+            </div>            
             { children }
         </div>
     )
