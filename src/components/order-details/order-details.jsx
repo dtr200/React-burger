e296f5dc2d49085a4fc56c4cda9469db53fc55fc
dtr200 <negevski@yandex.ru>
2021-11-15 23:39:58 +0300

    Sprint-1/step-2: OrderDetails style added

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -6,13 +6,13 @@ import done from '../../images/done.png';
 const OrderDetails = ({ num, description, extra }) => {
 
     return (
-        <>
-            <div className={styles.num}>{num}</div>
-            <p className={``}>идентификатор заказа</p>
-            <img src={done} alt={description} />
-            <div className={styles.description}>{description}</div>
-            <div className={styles.wait}>{extra}</div>
-        </>
+        <div className={`${styles.orderDetails} text`}>
+            <div className={`${styles.num} text_type_digits-large`}>{num}</div>
+            <p className={`text_type_main-medium`}>идентификатор заказа</p>
+            <img src={done} className={`${styles.image}`} alt={description} />
+            <div className={`${styles.description} text_type_main-default`}>{description}</div>
+            <div className={`${styles.wait} text_type_main-default text_color_inactive`}>{extra}</div>
+        </div>
     );
 }
 
