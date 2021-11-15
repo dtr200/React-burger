84182d820a378ceab0a9d221e643ad4245f3efd8
dtr200 <negevski@yandex.ru>
2021-11-15 23:45:01 +0300

    Sprint-1/step-2: OrderDetails component has been completed

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -6,12 +6,22 @@ import done from '../../images/done.png';
 const OrderDetails = ({ num, description, extra }) => {
 
     return (
-        <div className={`${styles.orderDetails} text`}>
-            <div className={`${styles.num} text_type_digits-large`}>{num}</div>
-            <p className={`text_type_main-medium`}>идентификатор заказа</p>
-            <img src={done} className={`${styles.image}`} alt={description} />
-            <div className={`${styles.description} text_type_main-default`}>{description}</div>
-            <div className={`${styles.wait} text_type_main-default text_color_inactive`}>{extra}</div>
+        <div className={`${styles.orderDetails} text mt-4 mb-15`}>
+            <div className={`${styles.num} text_type_digits-large`}>
+                {num}
+            </div>
+            <p className={`${styles.text} text_type_main-medium mt-8`}>
+                идентификатор заказа
+            </p>
+            <img src={done} 
+                 className={styles.image} 
+                 alt={description} />
+            <div className="text_type_main-default mb-2">
+                {description}
+            </div>
+            <div className="text_type_main-default text_color_inactive">
+                {extra}
+            </div>
         </div>
     );
 }
