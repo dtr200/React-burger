b06d4e6f3d4b8d166ef663cbd53782ffbeff3089
dtr200 <negevski@yandex.ru>
2021-12-04 21:09:16 +0300

    Sprint-2: redux installed, redux folder added

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
+import { compose, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+//const store = createStore(rootReducer, enhancer); 
 
 ReactDOM.render(
   <React.StrictMode>
