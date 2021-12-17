a84e8df9c04e1043632eb2ff36d6d07f2e3b4637
dtr200 <negevski@yandex.ru>
2021-12-17 23:17:41 +0300

    Sprint-3/step-1: LoginPage is done

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,18 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Input, Button } from 
+    '@ya.praktikum/react-developer-burger-ui-components';
 
-import { Input } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './login.module.css';
 
 const LoginPage = () => {
   return (
-    <main className={`${styles.main}`}>
-        <h1 className=''>Вход</h1>
-        <Input
-            type={'text'}
-            placeholder={'E-mail'}
-            name={'E-mail'}            
-            size={'default'}
-            />
+    <main className={styles.main}>
+        <section className={`${styles.container} text`}>
+            <h1 className={`${styles.title} text_type_main-medium`}>Вход</h1>
+            <div className={styles.inputLarge}>
+                <Input
+                    type={'email'}
+                    placeholder={'E-mail'}
+                    name={'E-mail'}            
+                    size={'default'}
+                    />
+            </div>
+            <div className={styles.inputLarge}>
+                <Input
+                    type={'password'}
+                    placeholder={'Пароль'}
+                    name={'password'}    
+                    icon={'ShowIcon'}        
+                    size={'default'}
+                    />
+            </div>
+            <div className='mt-6 mb-20'>
+                <Button type="primary" size="medium">Войти</Button>
+            </div>
+            <section className={styles.questions}>
+                <p className={`${styles.question} text_type_main-default mb-4`}>
+                    <span>Вы — новый пользователь? </span> 
+                    <Link to='/register'>Зарегистрироваться</Link>
+                </p>
+                <p className={`${styles.question} text_type_main-default`}>
+                    <span>Забыли пароль? </span>
+                    <Link to='/forgot-password'>Восстановить пароль</Link>
+                </p>   
+            </section>
+        </section>        
     </main>
   );
 }
