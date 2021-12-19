d05dad5a391892ecf9795d2269123889a5cde9dc
dtr200 <negevski@yandex.ru>
2021-12-19 22:18:07 +0300

    Sprint-3/step-1: header links is works now

--- src/components/nav-button/nav-button.jsx
+++ src/components/nav-button/nav-button.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { BurgerIcon, ListIcon, ProfileIcon } from 
 '@ya.praktikum/react-developer-burger-ui-components';
 import {
@@ -9,20 +10,26 @@ import {
 import styles from './nav-button.module.css';
 
 const NavButton = ({ title, logo, type, view }) => {
-    const dict = {
+    const dictLogoToImg = {
         burger: () => <BurgerIcon type={type} />,
         list: () => <ListIcon type={type} />,
         profile: () => <ProfileIcon type={type} />
-    }   
+    }
+
+    const dictTitleToLink = {
+        'Конструктор': '/',
+        'Лента заказов': '/',
+        'Личный кабинет': '/profile'
+    }
 
     const titleStyle = `text text_type_main-default pl-2
                        ${view ? styles.active : 'text_color_inactive'}`;
     
     return (
-        <a href='#' className={`${styles.navButton} pl-5 pr-5 pt-4 pb-4`}>
-            {dict[logo]()}
+        <Link to={dictTitleToLink[title]} className={`${styles.navButton} pl-5 pr-5 pt-4 pb-4`}>
+            {dictLogoToImg[logo]()}
             <span className={titleStyle}>{title}</span>
-        </a>
+        </Link>
     )
 }
 
