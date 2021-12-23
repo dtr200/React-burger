ca526df2ac077c22e1a67daab89ed957eca9cc3c
dtr200 <negevski@yandex.ru>
2021-12-23 14:40:59 +0300

    Sprint-3/step-1: fetchWithRefresh added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,40 @@ export const DEFAULT_ORDER_DATA = {
   description: 'Ваш заказ начали готовить', 
   extra: 'Дождитесь готовности на орбитальной станции'
 };
+
+export const setCookie = (name, value) => 
+    document.cookie = `${name}=${value}`;
+
+export const deleteCookie = (name) =>
+    document.cookie = `${name}=;Expires=${new Date(0).toUTCString()}`;
+
+const checkResponse = (res) => {
+  return res.ok ? 
+      res.json() :
+      res.json().then((err) => Promise.reject(err));
+}
+
+export const updateToken = async (url, options) => {
+  const res = await fetch(url, options);
+  return await checkResponse(res);
+}
+
+export const fetchWithRefresh = async (url, options) => {
+  try{
+      const res = await fetch(url, options);
+      return await checkResponse(res);
+  }
+  catch(err){
+      if(err.message === 'jwt expired'){
+          const updateData = await updateToken(url, options);
+          localStorage.setItem('refreshToken', updateData.refreshToken);
+          setCookie('accessToken', updateData.accessToken);
+          options.headers.authorization = updateData.accessToken;
+          const res = await fetch(url, options)
+          return await checkResponse(res);
+      }
+      else{
+          return Promise.reject(err);
+      }
+  }
+}
