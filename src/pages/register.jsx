288bfececd367c24e7a8aabcb66975e2cacecf28
dtr200 <negevski@yandex.ru>
2021-12-23 18:52:43 +0300

    Sprint-3/step-1: RegisterPage has been completed

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -5,14 +5,15 @@ import { registerNewUser } from '../services/actions/thunks';
 import { SET_NAME, SET_EMAIL, SET_PASSWORD } from '../services/actions/action-types';
 import { Input, Button } from 
     '@ya.praktikum/react-developer-burger-ui-components';
+import Spinner from '../components/spinner/spinner';
 
 import styles from './page.module.css';
 
 const RegisterPage = () => {
 
     const dispatch = useDispatch();
-    const { name, email, password } = 
-        useSelector(store => store.access.user);
+    const { user: {name, email, password}, registerRequest } = 
+        useSelector(store => store.access);
     const history = useHistory();
     const { state } = history.location;
 
@@ -38,59 +39,61 @@ const RegisterPage = () => {
     }
 
     return (
-        isAccessTokenExist ? (
-            <Redirect to={state?.from || '/'} />
-            ) : (
-            <main className={styles.main}>
-                <section className={`${styles.container} text`}>
-                    <h1 className={`${styles.title} text_type_main-medium`}>Регистрация</h1>
-                    <div className={styles.inputLarge}>
-                        <Input
-                            type={'text'}
-                            placeholder={'Имя'}
-                            name={'name'}            
-                            size={'default'}
-                            value={name}
-                            onChange={(e) => setValue(e)}
-                            />
-                    </div>
-                    <div className={styles.inputLarge}>
-                        <Input
-                            type={'email'}
-                            placeholder={'E-mail'}
-                            name={'email'}            
-                            size={'default'}
-                            value={email}
-                            onChange={(e) => setValue(e)}
-                            />
-                    </div>
-                    <div className={styles.inputLarge}>
-                        <Input
-                            type={'password'}
-                            placeholder={'Пароль'}
-                            name={'password'}    
-                            icon={'ShowIcon'}        
-                            size={'default'}
-                            value={password}
-                            onChange={(e) => setValue(e)}
-                            />
-                    </div>
-                    <div className='mt-6 mb-20'>
-                        <Button 
-                            type="primary" 
-                            size="medium"
-                            onClick={registerUser}>
-                            Зарегистрироваться
-                        </Button>
-                    </div>
-                    <section className={styles.questions}>
-                        <p className={`${styles.question} text_type_main-default`}>
-                            <span>Уже зарегистрированы? </span>
-                            <Link to='/login'>Войти</Link>
-                        </p>   
-                    </section>
-                </section>        
-            </main>
+        registerRequest ? (
+        <Spinner />
+        ) : ( isAccessTokenExist ) ? (
+        <Redirect to={state?.from || '/'} />
+        ) : (
+        <main className={styles.main}>
+            <section className={`${styles.container} text`}>
+                <h1 className={`${styles.title} text_type_main-medium`}>Регистрация</h1>
+                <div className={styles.inputLarge}>
+                    <Input
+                        type={'text'}
+                        placeholder={'Имя'}
+                        name={'name'}            
+                        size={'default'}
+                        value={name}
+                        onChange={(e) => setValue(e)}
+                        />
+                </div>
+                <div className={styles.inputLarge}>
+                    <Input
+                        type={'email'}
+                        placeholder={'E-mail'}
+                        name={'email'}            
+                        size={'default'}
+                        value={email}
+                        onChange={(e) => setValue(e)}
+                        />
+                </div>
+                <div className={styles.inputLarge}>
+                    <Input
+                        type={'password'}
+                        placeholder={'Пароль'}
+                        name={'password'}    
+                        icon={'ShowIcon'}        
+                        size={'default'}
+                        value={password}
+                        onChange={(e) => setValue(e)}
+                        />
+                </div>
+                <div className='mt-6 mb-20'>
+                    <Button 
+                        type="primary" 
+                        size="medium"
+                        onClick={registerUser}>
+                        Зарегистрироваться
+                    </Button>
+                </div>
+                <section className={styles.questions}>
+                    <p className={`${styles.question} text_type_main-default`}>
+                        <span>Уже зарегистрированы? </span>
+                        <Link to='/login'>Войти</Link>
+                    </p>   
+                </section>
+            </section>        
+        </main>
         )
     );
 }
