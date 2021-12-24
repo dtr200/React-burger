8d62b6f943705f7f8d47ffce6beaca658b787871
dtr200 <negevski@yandex.ru>
2021-12-24 20:19:12 +0300

    Sprint-3/step-1: HomePage component has been cleared from Modal code

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,34 +6,22 @@ import BurgerConstructor from "../components/burger-constructor/burger-construct
 import BurgerIngredients from "../components/burger-ingredients/burger-ingredients";
 import ErrorIndicator from '../components/error-indicator/error-indicator';
 import Spinner from '../components/spinner/spinner';
-import Modal from '../components/modal/modal';
-import OrderDetails from '../components/order-details/order-details';
-import IngredientDetails from '../components/ingredient-details/ingredient-details';
 import { INGREDIENTS_URL } from 
   '../utils/constants';
 import { getIngredients } from '../services/actions/thunks';
 
 import styles from './home.module.css';
 
-const HomePage = ({ onModalClose }) => {
+const HomePage = () => {
   const dispatch = useDispatch();
   const {
     ingredientsRequest, 
     ingredientsFailed
   } = useSelector(store => store.ingredients);
 
-  const {
-    modalVisible,
-    modalMode,
-    hasModalError
-  } = useSelector(store => store.modal);
-  const { orderFailed } = useSelector(store => store.order);
-
   useEffect(() => {
     dispatch(getIngredients(INGREDIENTS_URL));
-  }, []);
-
-  
+  }, []);  
 
   return (
     <main className={`${styles.main}`}>
@@ -43,15 +31,6 @@ const HomePage = ({ onModalClose }) => {
             <DndProvider backend={HTML5Backend}>
             <BurgerIngredients />
             <BurgerConstructor />
-            {/* { (modalVisible || orderFailed) &&
-                <Modal onClose={onModalClose}>
-                { 
-                    (hasModalError || orderFailed) ?
-                    <ErrorIndicator /> :             
-                    (modalMode === 'ingredient' ? 
-                    <IngredientDetails /> : <OrderDetails />)
-                }
-                </Modal> } */}
             </DndProvider>
         </>
         }
