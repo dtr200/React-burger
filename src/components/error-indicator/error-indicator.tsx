1fa9d476f3b02f79591f5184efd3abd1b0f80215
dtr200 <negevski@yandex.ru>
2022-01-18 13:12:24 +0300

    Sprint-4: ErrorIndicator typed

--- src/components/error-indicator/error-indicator.tsx
+++ src/components/error-indicator/error-indicator.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
 import styles from './error-indicator.module.css';
 
-const ErrorIndicator = () => {
+const ErrorIndicator: FunctionComponent = () => {
     return (
         <div className={styles.errorIndicator}>
             <span>Ошибка</span>
