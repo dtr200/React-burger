510535251a37b11478e3879da3906483bfdcd3f4
dtr200 <negevski@yandex.ru>
2022-01-20 06:53:52 +0300

    Sprint-4: Modal switched to ts

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, FunctionComponent, KeyboardEventHandler } from "react";
 import ReactDOM from 'react-dom';
 import ModalOverlay from '../modal-overlay/modal-overlay';
-import PropTypes from 'prop-types';
 import { CloseIcon } from 
     '@ya.praktikum/react-developer-burger-ui-components';
 
 import styles from './modal.module.css';
 
-const root = document.getElementById('react-modals');
+const root = document.getElementById('react-modals')!;
 
-const Modal = ({ children, onClose }) => {
+type TModalProps = {
+    onClose: () => void;
+}
+
+const Modal: FunctionComponent<TModalProps> = ({ children, onClose }) => {
   
-    const handleKeyPress = (e) => {
+    const handleKeyPress: (e: KeyboardEvent) => void = (e) => {
         if(e.key === 'Escape')
             onClose();
     }
@@ -26,7 +29,7 @@ const Modal = ({ children, onClose }) => {
         <>
             <ModalOverlay onClose={onClose} />
             <div className={`${styles.modal} p-10 pb-15`} 
-                 onKeyDown={handleKeyPress} tabIndex='0'>
+                 /* onKeyDown={handleKeyPress} */ tabIndex={0}>
                 <div className={styles.close}>
                     <CloseIcon onClick={onClose} type="primary" />       
                 </div>               
@@ -35,11 +38,6 @@ const Modal = ({ children, onClose }) => {
         </>
     )
     return ReactDOM.createPortal(content, root);
-}
-
-Modal.propTypes = {
-    children: PropTypes.element.isRequired,
-    onClose: PropTypes.func.isRequired
 }
 
 export default Modal;
