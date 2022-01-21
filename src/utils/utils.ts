1892ed8f8fc1a4d1cf0d5e5e007104031a6f5799
dtr200 <negevski@yandex.ru>
2022-01-21 21:09:03 +0300

    Sprint-4: utils file has been typed

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,15 +1,22 @@
 import { BASE_URL } from "./constants";
+import { TDict } from './types';
 
-export const setCookie = (name, value) => 
+type TFetchOptions = {
+  method: string;
+  headers: TDict<string>;
+  body: string;
+}
+
+export const setCookie = (name: string, value: string) => 
     document.cookie = `${name}=${value}`;
 
-export const deleteCookie = (name) =>
+export const deleteCookie = (name: string) =>
     document.cookie = `${name}=;Expires=${new Date(0).toUTCString()}`;
 
-export const checkResponse = (res) => {
+export const checkResponse = (res: any) => {
   return res.ok ? 
       res.json() :
-      res.json().then((err) => Promise.reject(err));
+      res.json().then((err: any) => Promise.reject(err));
 }
 
 export const updateToken = () => {
@@ -25,14 +32,14 @@ export const updateToken = () => {
   .then(checkResponse);
 }
 
-export const fetchWithRefresh = async (url, options) => {
+export const fetchWithRefresh = async (url: string, options: TFetchOptions) => {
   try{
       const res = await fetch(url, options);
       return await checkResponse(res);
   }
-  catch(err){
+  catch(err: any){
       if(err.message === 'jwt expired'){
-          const updateData = await updateToken(url, options);
+          const updateData = await updateToken();
           localStorage.setItem('refreshToken', updateData.refreshToken);
           setCookie('accessToken', updateData.accessToken);
           options.headers.authorization = updateData.accessToken;
