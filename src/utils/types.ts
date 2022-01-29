57f353ada7bb9260d1ad3d94781b0a201ae19f9a
dtr200 <negevski@yandex.ru>
2022-01-30 00:19:29 +0300

    Sprint-5: extra types removed

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -25,30 +25,12 @@ export type TDict<T> = {
 export type TRegisterUserData = {
     name: string;
     email: string;
-    password: string;
 }
-export type TUpdateUserData = {
-    login: string;
-    password: string;
-}
-export type TUserName = {
-    name: string;
-}
-export type TUserLogin = {
-    login: string;
-}
-export type TUserEmail = {
-    email: string;
-}
-export type TUserPassword = {
-    password: string;
-}
-export type TUserNewPassword = {
-    newPassword: string;
-}
+
 type TOrderField = {
     number: number;
 }
+
 export type TOrderResponseData = {
     success: boolean;
     name: string;
