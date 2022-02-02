b8c8dd9065c1728f04e5772a07fe7388e3e4f06a
dtr200 <negevski@yandex.ru>
2022-02-03 00:39:44 +0300

    Sprint-5: soketMiddleware modified to work with payload url

--- src/components/full-feed/full-feed.tsx
+++ src/components/full-feed/full-feed.tsx
@@ -1,21 +1,33 @@
 import React, { useEffect, FunctionComponent } from "react";
 import OrderBlock from '../order-block/order-block';
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 import { WS_CONNECTION_START } from '../../services/action-constants/ws';
+import { WS_ORDERS } from '../../utils/constants';
 
 import Spinner from '../spinner/spinner';
 
 import styles from './full-feed.module.css';
 
 const FullFeed: FunctionComponent = () => {
+    const location = useLocation();
+
+    const wsUrl = `${WS_ORDERS}/all`;
+    const isTokenExist = document.cookie.match(/(accessToken=)(.+)/);
+    let wsUrlToken = '';
+    if(isTokenExist)
+        wsUrlToken = `${WS_ORDERS}?token=${
+            document.cookie.match(/(accessToken=)(.+)/)![2].split(' ')[1]}`;
 
     const dispatch = useDispatch();
 
     const { orders } = useSelector((store: any) => store.ws);
 
-    console.log(orders)
     useEffect(() => {
-        dispatch({ type: WS_CONNECTION_START });
+        dispatch({ 
+            type: WS_CONNECTION_START,
+            payload: location.pathname === '/feed' ? wsUrl : wsUrlToken
+        });
     }, [])
 
     return (
