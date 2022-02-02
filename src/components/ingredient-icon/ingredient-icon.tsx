caa99a943031339f48098aa968cb95e66cea9555
dtr200 <negevski@yandex.ru>
2022-02-02 16:30:10 +0300

    Sprint-5: burger ingredients icons works

---
+++ src/components/ingredient-icon/ingredient-icon.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react";
+
+import styles from './ingredient-icon.module.css';
+
+type TIngredientIconProps = {
+    image: string;
+    shiftRatio: number;
+    rest: number | boolean;
+}
+
+const IngredientIcon: FunctionComponent<TIngredientIconProps> = ({ image, shiftRatio, rest }) => {
+
+    const shift = shiftRatio && styles[`shift-${shiftRatio}`];
+
+    return (
+        <li className={`${styles.icon} ${shift}`}>
+            <div className={`${styles.overlay}`}>
+                { 
+                    rest && (
+                    <div className={`${styles.rest} text text_type_main-default`}>
+                        {`+${rest}`}
+                    </div>
+                    ) 
+                }
+                <img src={image} alt="ingredient" width="112" height="56"/>
+            </div>        
+        </li>
+    )
+}
+
+export default IngredientIcon;
--- src/components/order-block/order-block.tsx
+++ src/components/order-block/order-block.tsx
@@ -1,4 +1,6 @@
 import React, { FunctionComponent } from "react";
+import { useSelector } from "react-redux";
+import IngredientIcon from '../ingredient-icon/ingredient-icon';
 
 import { CurrencyIcon } from 
     '@ya.praktikum/react-developer-burger-ui-components';
@@ -7,28 +9,81 @@ import styles from './order-block.module.css';
 type TOrderBlockProps = {
     number: number;
     createdAt: string;
+    name: string;
     status: string;
     ingredients: string[];
+    nostatus: boolean;
 }
 
 const OrderBlock: FunctionComponent<TOrderBlockProps> = 
-    ({ number, createdAt, status, ingredients }) => {
+    ({ number, name, createdAt, status, ingredients, nostatus }) => {
 
+    const { ingredientsData } = useSelector((store: any) => store.ingredients);
+
+    const price = ingredients.reduce((accum: number, id: string) => 
+        accum + ingredientsData.find((item: any) => item._id === id).price, 0);
+
+    const images = Array.from(
+        new Set(
+            ingredients.map((id: string) =>
+                ingredientsData.find((item: any) => item._id === id).image_mobile)
+            )
+        );
+    
+    const getTime = () => {
+        const created = new Date(createdAt);
+        const time = created.toTimeString().split(' ')[0];
+
+        const todayDate = new Date();
+        const msInDay = 24 * 3600 * 1000;
+
+        const digitsToWords = ['Сегодня', 'Вчера'];
+        const daysBefore = Math.floor((todayDate.getTime() - created.getTime())/msInDay);
+
+        const getDayForm = (day: number): string => {
+            return day % 10 === 1 ? 'день' :
+                day % 10 >= 2 && day % 10 <= 4 && day !== 12 && day !== 13 && day !== 14 ? 
+                'дня' : 'дней'; 
+        }
+
+        const getDay = () => {
+            return daysBefore < 2 ? 
+                digitsToWords[daysBefore] : 
+                `${daysBefore} ${getDayForm(daysBefore)} назад`;
+        }
+
+        return `${getDay()} ${time} i-GMT+3`;
+    }
+    
     return (
-        <section className={styles.orderBlock}>
+        <li className={`${styles.orderBlock} text text_type_digits-default`}>
             <div className={styles.meta}>
-                <span className={``}>{number}</span>
-                <span className={``}>{createdAt}</span>
+                <span className={``}>{`#${number}`}</span>
+                <span className={`${styles.date} text_type_main-default`}>{getTime()}</span>
             </div>
-            <h2 className={styles.title}></h2>
-            <p className={styles.status}>{status}</p>
-            <div>
+            <h2 className={`${styles.title} text_type_main-medium`}>{name}</h2>
+            <p className={styles.status}>{!nostatus && status}</p>
+            <div className={styles.info}>
+                <ul className={styles.icons}>
+                    {
+                        images.map((image: string, i: number) => {
+                            if(i >= 6) return;
+                            return (
+                                <IngredientIcon 
+                                    image={image} 
+                                    key={i} 
+                                    shiftRatio={i} 
+                                    rest={i === 5 && images.length - 6} />
+                            )
+                        })
+                    }
+                </ul>
                 <div className={styles.priceBlock}>
-                    <span className={styles.price}></span>
+                    <span className={styles.price}>{price}</span>
                     <CurrencyIcon type="secondary" />
                 </div>
             </div>
-        </section>
+        </li>
     )
 }
 
