5ee55848511589562b55036ba400ae7908bf4b92
dtr200 <negevski@yandex.ru>
2022-02-03 00:40:48 +0300

    Sprint-5: OrderHistoryPage is ready

--- src/pages/order-history.tsx
+++ src/pages/order-history.tsx
@@ -1,8 +1,5 @@
-import React, { useEffect, FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
 import FullFeed from '../components/full-feed/full-feed';
-
-import { useDispatch, useSelector } from "react-redux";
-import { WS_CONNECTION_START } from '../services/action-constants/ws';
 
 import styles from './profile.module.css';
 
