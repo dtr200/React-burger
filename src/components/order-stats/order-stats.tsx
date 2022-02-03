cae1be836b2dc755265f07302d208cf646273a99
dtr200 <negevski@yandex.ru>
2022-02-03 14:54:39 +0300

    Sprint-5: OrderStats route added

---
+++ src/components/order-stats/order-stats.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from "react";
+import { useSelector } from "react-redux";
+import { useLocation, useRouteMatch } from "react-router-dom";
+
+import styles from './order-stats.module.css';
+
+type TOrderStatsProps = {
+    
+}
+
+const OrderStats: FunctionComponent<TOrderStatsProps> = () => {
+    
+    return (
+        <>
+            OrderStats
+        </>
+    )
+}
+
+export default OrderStats;
