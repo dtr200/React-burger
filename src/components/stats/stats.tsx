7f6a56f4366917e8f90cd616e7a74ccc4d680db0
dtr200 <negevski@yandex.ru>
2022-02-04 15:56:23 +0300

    Sprint-5: Stats typed

--- src/components/stats/stats.tsx
+++ src/components/stats/stats.tsx
@@ -1,14 +1,16 @@
 import React, { FunctionComponent } from "react";
-import { useSelector } from "react-redux";
+import { useSelector } from "../../services/types/hooks";
+import { TWSOrder } from '../../utils/types';
 
 import styles from './stats.module.css';
 
 const Stats: FunctionComponent = () => {
 
-    const { orders, total, totalToday } = useSelector((store: any) => store.ws);
+    const { orders, total, totalToday }: 
+    { orders: TWSOrder[], total: number, totalToday: number } = useSelector(store => store.ws);
 
-    const doneOrders = orders.filter((order: any) => order.status === 'done');
-    const inProgressOrders = orders.filter((order: any) => order.status === 'created');
+    const doneOrders: TWSOrder[] = orders.filter(order => order.status === 'done');
+    const inProgressOrders: TWSOrder[] = orders.filter(order => order.status === 'created');
 
     const getTotalSection = (title: string, total: number) => {
         return (
@@ -19,13 +21,13 @@ const Stats: FunctionComponent = () => {
         )
     }
 
-    const getNumberSection = (title: string, orders: any) => {
+    const getNumberSection = (title: string, orders: TWSOrder[]) => {
         return (
             <div className={styles.numbers}>
                     <h3 className={`text_type_main-medium mb-6 mt-1`}>{title}</h3>
                     <ul className={styles.allNumbers}>
                         {
-                            orders.map((order: any, i: number) => (                               
+                            orders.map((order: TWSOrder, i: number) => (                               
                                 <li className={`${styles.num} ${title === 'В работе:' ? 
                                     styles.inProgress : styles.done} text_type_digits-default`} key={i}>
                                     {order.number}
