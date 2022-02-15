f9ec87d719db0276a56a51aa21dd3a229829570e
dtr200 <negevski@yandex.ru>
2022-02-15 14:15:45 +0300

    Sprint-6/step-1: test cases has been updated and splitted to login test and order tests

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('should login successfylly', () => {
-    before(() => {
+    it('should login and get to the main page', () => {
         cy.visit('http://localhost:3000/login')
         cy.get('[name="email"]')
           .type('jaxer@list.ru')
@@ -10,74 +10,5 @@ describe('should login successfylly', () => {
         cy.get('button')
           .click()  
         cy.contains('Соберите бургер')
-    })
-
-    it('should open the modal', () => {
-        cy.get('[alt="Соус фирменный Space Sauce"]')
-          .click()
-        cy.get('p')
-          .contains('Соус фирменный Space Sauce')
-        cy.get('span')
-          .contains('Белки, г')
-    })
-
-    it('should close the modal', () => {
-        cy.get('h3')
-          .parent()
-          .parent()
-          .find('div')
-          .first()
-          .click()
-    })
-
-    it('should drag & drop an ingredients', () => {
-        cy.get('[alt="Флюоресцентная булка R2-D3"]')
-          .trigger('dragstart')
-        cy.get('main section')
-          .last()  
-          .trigger('drop')
-        cy.get('[alt="Филе Люминесцентного тетраодонтимформа"]')
-          .trigger('dragstart')
-        cy.get('main section')
-          .last()
-          .find('ul')
-          .eq(1) 
-          .trigger('drop')
-        cy.get('[alt="Биокотлета из марсианской Магнолии"]')
-          .trigger('dragstart')
-        cy.get('main section')
-          .last()
-          .find('ul')
-          .eq(1) 
-          .trigger('drop')
-        cy.get('[alt="Соус традиционный галактический"]')
-          .trigger('dragstart')
-        cy.get('main section')
-          .last()
-          .find('ul')
-          .eq(1) 
-          .trigger('drop')
-    });
-
-    it('should drag & drop works in constructor side', () => {
-        cy.get('main section')
-          .last()
-          .find('[alt="Соус традиционный галактический"]')
-          .trigger('dragstart')
-        cy.get('[alt="Филе Люминесцентного тетраодонтимформа"]')
-          .eq(1)
-          .trigger('drop')
-    })
-    it('should delete one ingredient', () => {
-        cy.get('main section')
-          .last()
-          .find('ul')
-          .eq(1)
-          .find('svg')
-          .eq(2)
-          .click()
-    })
-    it('should checkout successfylly', () => {
-        
     })
 })
